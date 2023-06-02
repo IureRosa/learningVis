c750fed8e4bb7cb2b5df167c319ee2394dd6ecdf
IureRosa <iurengineer@gmail.com>
2023-06-02 18:28:57 -0300

    add function save_plot for save results obtained for each trainning

--- agents/basicAlgs.py
+++ agents/basicAlgs.py
@@ -109,6 +109,21 @@ def plot_results(q_rewards, q_success_rate, sarsa_rewards, sarsa_success_rate):
 
     plt.tight_layout()
     st.pyplot(fig)
+    save_plot_as_png(fig)
+
+def save_plot_as_png(figure):
+    
+    script_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    parent_directory = "results"
+    sub_directory = f"{script_name}_{timestamp}"
+    directory = os.path.join(parent_directory, sub_directory)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    filename = os.path.join(directory, f"{script_name}_{timestamp}.png")
+    figure.savefig(filename, format='png')
+    plt.close(figure)
+    print(f"Plot saved as {filename}")
 
 # Interface do Streamlit
 def main():
